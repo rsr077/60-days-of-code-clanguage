caef269f8022b8f8585a62bf8731003517dfa57e
rsr077 <yourfriend77777777@gmail.com>
2024-04-18 21:50:38 +0530

    second

---
+++ Day_02/Scond.c
@@ -0,0 +1,10 @@
+// Write a program to change data type of results obtained by divison operations.
+
+#include<stdio.h>
+void main(){
+    printf("\n Division operation    Result");
+    printf("\n Two Integers(5&2) :%d",5/2);
+    printf("\nOne int & one float(5.5 &2): %g",5.5/2);
+    printf("\ntwo Integers(5&2)   :%g",(float)5/2);
+    
+}
