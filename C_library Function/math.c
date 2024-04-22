5cbec493c71eb55821c87dd1121c11b78015127d
rsr077 <yourfriend77777777@gmail.com>
2024-04-22 13:02:53 +0530

    math

---
+++ C_library Function/math.c
@@ -0,0 +1,10 @@
+#include<math.h>
+#include<stdio.h>
+
+int main(){
+    double number,squareRoot;
+    number=12.5;
+    squareRoot=sqrt(number);
+printf("Square root of %.2lf=%.3lf",number,squareRoot);
+return 0;
+}
