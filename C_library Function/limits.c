a4c1d4484d79124973530c4ed014df638f80aa01
rsr077 <yourfriend77777777@gmail.com>
2024-04-22 14:31:20 +0530

    limits

---
+++ C_library Function/limits.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main(){
+    printf("Number of bits in a byte %d\n",CHAR_BIT);
+    printf("Mininum value of SIGNES CHAR =%d\n",SCHAR_MIN);
+    printf("Maximum value of SIGNES CHAR =%d\n",SCHAR_MAX);
+    printf("Maximum value of UNSIGNES CHAR =%d\n",UCHAR_MAX);
+    printf("Mininum value of SHORT INT  =%d\n",SHRT_MIN);
+     printf("Miximum value of SHORT INT  =%d\n",SHRT_MAX);
+      printf("Mininum value of   INT =%d\n",INT_MIN);
+       printf("Maximum value of  INT  =%d\n",INT_MAX);
+        printf("Mininum value of   CHAR =%d\n",CHAR_MIN);
+         printf("Maximum value of   CHAR =%d\n",CHAR_MAX) ;
+          printf("Mininum value of   LONG =%d\n",LONG_MIN);
+           printf("Mininum value of   LONG =%d\n",LONG_MAX);
+
+
+
+    
+}
+
