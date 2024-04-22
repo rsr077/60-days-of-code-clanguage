c23efce8a06fc8eb2324b56378bcfbdb5e8975df
rsr077 <yourfriend77777777@gmail.com>
2024-04-22 13:45:46 +0530

    con

---
+++ C_library Function/cos.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+#define PI 3.141592654
+
+int main(){
+    double arg =30, result;
+    arg = (arg * PI)/180;
+    result = cos(arg);
+    printf("cos of %.2lf radian =%.2lf",arg,result);
+    return 0;
+}
