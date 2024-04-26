dbe7596df406e308d4593bd98b57224ee3ebe1d0
rsr077 <yourfriend77777777@gmail.com>
2024-04-26 19:18:04 +0530

    break

---
+++ Day_04_Keyword/break_and_continue.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=10;i++)
+    {
+        if(i==3)
+        {
+            continue;
+        }
+        {
+            if(i==6)
+            {
+                break;
+            }
+            printf("%d",i);
+
+        }
+    }
+    return 0;
+}
