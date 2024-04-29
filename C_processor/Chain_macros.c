2c3f5fd1e3d9153f4f3e4b391c1a8dc97c830df1
rsr077 <yourfriend77777777@gmail.com>
2024-04-29 20:00:22 +0530

    Chian

---
+++ C_processor/Chain_macros.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+// Macro Definiton 
+#define X FOLLOWERS
+#define fOLLOWERS 65
+
+int main(){
+    printf("Rajeev Singh Rajput have %dK " "Followers on Instagram","INSTAGRAM");
+    return 0;
+} 
---
+++ C_processor/object.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+// Macro definition 
+#define DATE 31
+int main (){
+    printf("Today I'm complet my 42 days of code"  "upto %d -March-2024",DATE );
+    return 0;
+}
