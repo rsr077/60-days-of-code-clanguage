f1a4455a30379a76e3680a46a17297b392e7975f
rsr077 <yourfriend77777777@gmail.com>
2024-05-18 14:59:03 +0530

    new

---
+++ Learining/Data_types/enum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+typedef enum {false,true}bool;
+int main(){
+    bool a=true;
+    bool b=false;
+ printf("True:%d\n",a);
+ printf("False:%d\n",b);
+ return 0;
+}
---
+++ Learining/Data_types/stdbool.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    bool a=true;
+    bool b=false;
+ printf("True:%d\n",a);
+ printf("False:%d\n",b);
+ return 0;
+}
