9dacff23574423afccb17402fb65234f26121419
rsr077 <yourfriend77777777@gmail.com>
2024-05-21 16:02:51 +0530

    new

---
+++ C_programing_tutorial/loop_control_staements/quick_quize.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int i=10;
+    while(i<21)
+    {
+        
+            if(i>=10){
+
+            }
+        
+        printf("The value of i is %d\n",i);
+        i++;
+    }
+    return 0;
+}
